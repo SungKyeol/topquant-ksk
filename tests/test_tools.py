8a308a8ae70047604585cd8eb9d8bcecf27ac9b3
SungKyeol <104804195+SungKyeol@users.noreply.github.com>
2026-04-22 14:03:00 +0900

    test: tickers without PR or TR are skipped

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -249,3 +249,21 @@ class TestReconstructStaleTrWithPr:
         result = reconstruct_stale_tr_with_pr(df, verbose=False)
 
         pd.testing.assert_frame_equal(result, df)
+
+    def test_ticker_missing_tr_is_skipped(self):
+        idx = pd.date_range('2000-01-03', periods=5, freq='B')
+        pr = pd.Series([100.0, 101.0, 102.0, 103.0, 104.0], index=idx)
+        df = _make_index_df({('T1', 'Only PR'): {'FG_PRICE': pr}})
+
+        result = reconstruct_stale_tr_with_pr(df, verbose=False)
+
+        pd.testing.assert_frame_equal(result, df)
+
+    def test_ticker_missing_pr_is_skipped(self):
+        idx = pd.date_range('2000-01-03', periods=5, freq='B')
+        tr = pd.Series([500.0, 500.0, 500.0, 520.0, 530.0], index=idx)
+        df = _make_index_df({('T1', 'Only TR'): {'FG_TOTAL_RET_IDX': tr}})
+
+        result = reconstruct_stale_tr_with_pr(df, verbose=False)
+
+        pd.testing.assert_frame_equal(result, df)
